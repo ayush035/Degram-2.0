a5f249bf4d52c8e74e86374fc848074fbd763c64
Ayush Gupta <ayushguptaknp2080@gmail.com>
2024-01-21 20:31:27 +0530

    main

---
+++ components/Navbar.js
@@ -0,0 +1,47 @@
+import Link from "next/link";
+import { useAccount } from "wagmi";
+import { ConnectKitButton } from "connectkit";
+
+export default function Navbar() {
+
+    const MyComponent = () => {
+        const { address, isConnecting, isDisconnected } = useAccount();
+        if (isConnecting) return <div>Connecting...</div>;
+        if (isDisconnected) return <div>Disconnected</div>;
+        return <div>Connected Wallet: {address}</div>;
+      };
+
+    return (
+        
+            <>
+            <nav className=' flex justify-between h-12 text-white bg-black   font-bold' >
+                <span className='mx-20 my-2 flex text-white text-2xl  '><Link href={"/"}>Degram</Link></span>
+                <ul className= 'px-2 py-3 flex space-x-10 mx-12 '>
+
+        <div className='hover:text-white delay-50  text-md font-semibold font-mono'>
+        
+            <Link href="/explore">Explore</Link></div>
+        <div className='hover:text-white delay-50  text-md font-semibold font-mono'>
+        
+            <Link href="/search">Search</Link></div>
+
+            <div className='hover:text-white delay-50  text-md font-semibold font-mono'>
+        
+            <Link href="/post">Post</Link></div>
+
+        <div className='hover:text-white delay-50 text-md font-semibold font-mono '>
+        
+            <Link href="/profile">Profile</Link></div>
+              {/* <div className=' mx-2 my-2 '> */}   
+              <div className='hover:text-white delay-50 text-lg font-semibold font-mono '>
+        
+</div>                 
+<ConnectKitButton />
+
+            </ul>
+        </nav>
+        {/* <hr/> */}
+        </>
+        
+        );
+        }
---
+++ pages/_app.js
@@ -0,0 +1,32 @@
+import { WagmiConfig, createConfig } from "wagmi";
+import { ConnectKitProvider, ConnectKitButton, getDefaultConfig } from "connectkit";
+import  { AppProps } from 'next/app';
+import '@/styles/globals.css'
+
+
+const config = createConfig(
+  getDefaultConfig({
+    // Required API Keys
+    alchemyId: process.env.ALCHEMY_ID, // or 
+    walletConnectProjectId: process.env.WALLETCONNECT_PROJECT_ID,
+
+    // Required
+    appName: "Degram",
+
+    // Optional
+    appDescription: "Your App Description",
+    appUrl: "https://family.co", // your app's url
+    appIcon: "https://family.co/logo.png", // your app's icon, no bigger than 1024x1024px (max. 1MB)
+  }),
+);
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <WagmiConfig config={config}>
+      <ConnectKitProvider>
+        <Component {...pageProps} />
+      </ConnectKitProvider>
+    </WagmiConfig>
+  );
+}
+export default MyApp;
